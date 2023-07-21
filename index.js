89cc799f325c9a7bab36fbac55cd4ec08add3b18
Dheeraj Rao Sainala <james@inboundpartners.com>
2023-07-22 01:25:10 +0530

    hello

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 // serve up production assets
-app.use(express.static("build"));
+app.use(express.static("buildOne"));
 // let the react app to handle any unknown routes
 // serve up the index.html if express does'nt recognize the route
 const path = require("path");
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "buildOne", "index.html"));
 });
 // if not in production use the port 5000
 const PORT = process.env.PORT || 3000;
